78c650ef2025a4743160727dfe4e821ee3945e21
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-08-03 16:57:02 -0500

    Adding operators folder

---
+++ operators/arithmeticOperators.dart
@@ -0,0 +1,10 @@
+void main() {
+  int suma = 5 + 6 + 7 + 8;
+  double substraction = 6 - 7 + 8 - 5.6;
+  int multiplication = 5 * 8;
+  double division = 7 / 7;
+  double modulo = 5 % 3;
+
+  print(modulo);
+  print(suma);
+}
